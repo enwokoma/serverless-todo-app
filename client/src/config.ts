a821b034ec23b9358dc07ef4c6a09766650a0e51
Emmanuel Nwokoma <nwokoma@gigabytedevelopersinc.com>
2022-10-03 05:37:22 +0100

    Initial commit

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'n81vsd9wke';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'gigabytedevelopersinc.auth0.com',                   // Auth0 domain
+  clientId: 'Nn8iHHoYMMH8COaqvpmB5t2iJC7uk7xV',             // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+};
